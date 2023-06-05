8b15c1122065f32b46953f69ab3ae4c7e4c36eae
Michael Linhardt <linhardt.michael@gmail.com>
2023-06-05 20:48:19 +0700

    package installation with autocompletion workinf

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+import Application from '@michaellinhardt/restfulapi'
 
 import config from './config/index.js'
+console.debug('yolo', config.env.port)
 
-console.debug('yolo', config)
+class MyServer extends Application {
+
+	constructor () {
+
+		super()
+		this.test()
+		console.debug(this.config.fake.fake)
+
+	}
+
+}
+
+const application = new MyServer()
+
+export default application
